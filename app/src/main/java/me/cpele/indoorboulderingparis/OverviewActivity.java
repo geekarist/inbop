490fc27d240a0faa7d946ebf88ebcf1e68a9c689
cpcp <christophe.pele@gmail.com>
2016-10-12 07:57:41 +0200

    Initial import

---
+++ app/src/main/java/me/cpele/indoorboulderingparis/OverviewActivity.java
@@ -0,0 +1,15 @@
+package me.cpele.indoorboulderingparis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+public class OverviewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_overview);
+    }
+}
