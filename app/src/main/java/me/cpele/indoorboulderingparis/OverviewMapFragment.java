f4c4afd60338d6e579ad54c2bebecd37c67b6496
cpcp <christophe.pele@gmail.com>
2016-10-14 07:57:45 +0200

    Center map on Paris

--- app/src/main/java/me/cpele/indoorboulderingparis/OverviewMapFragment.java
+++ app/src/main/java/me/cpele/indoorboulderingparis/OverviewMapFragment.java
@@ -3,11 +3,15 @@ package me.cpele.indoorboulderingparis;
 import android.app.Activity;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 public class OverviewMapFragment extends MapFragment implements OnMapReadyCallback {
+
+    private static final String TAG = OverviewMapFragment.class.getSimpleName();
 
     @Override
     public void onAttach(Activity activity) {
@@ -19,6 +23,8 @@ public class OverviewMapFragment extends MapFragment implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        Log.d(getClass().getSimpleName(), "TODO: Center map on Paris area");
+        Log.d(TAG, "Center map on Paris area");
+        LatLng paris = new LatLng(48.853861, 2.3283237);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(paris, 12));
     }
 }
