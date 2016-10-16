e7dc82a7c694b87005df22ae26333dd93d2bf695
cpele <cpele@xebia.fr>
2016-10-16 12:41:56 +0200

    Add splash screen

---
+++ app/src/main/java/me/cpele/indoorboulderingparis/SplashActivity.java
@@ -0,0 +1,15 @@
+package me.cpele.indoorboulderingparis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, PlacesActivity.class));
+        finish();
+    }
+}
