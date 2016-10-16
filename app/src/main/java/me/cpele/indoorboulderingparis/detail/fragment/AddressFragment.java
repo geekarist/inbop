4c5d9619f708ad0fb5841373b919fe6cbe4620cd
cpele <cpele@xebia.fr>
2016-10-16 18:01:14 +0200

    Move address and image to separate fragment

---
+++ app/src/main/java/me/cpele/indoorboulderingparis/detail/fragment/AddressFragment.java
@@ -0,0 +1,20 @@
+package me.cpele.indoorboulderingparis.detail.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.cpele.indoorboulderingparis.R;
+
+public class AddressFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_address, container);
+    }
+}
