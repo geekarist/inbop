0dd58809855c735aa2b7089b8914c045c6fe07a5
cpcp <geekarist@users.noreply.github.com>
2017-07-14 19:11:50 +0200

    View fetched graph info

--- app/src/main/java/me/cpele/inbop/experiment/ExperimentActivity.java
+++ app/src/main/java/me/cpele/inbop/experiment/ExperimentActivity.java
@@ -3,13 +3,16 @@ package me.cpele.inbop.experiment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import me.cpele.inbop.BuildConfig;
@@ -29,6 +32,9 @@ import static java.lang.String.format;
 
 public class ExperimentActivity extends AppCompatActivity {
 
+    @BindView(R.id.experiment_tv_graph_info)
+    TextView mGraphInfoText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,12 +47,8 @@ public class ExperimentActivity extends AppCompatActivity {
     @OnClick(R.id.experiment_bt_get_from_graph)
     void onClickGetFromGraph() {
 
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-            @Override
-            public void log(String message) {
-                Log.d(ExperimentActivity.this.getClass().getSimpleName(), message);
-            }
-        });
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(
+                message -> Log.d(ExperimentActivity.this.getClass().getSimpleName(), message));
 
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
@@ -67,13 +69,13 @@ public class ExperimentActivity extends AppCompatActivity {
         graphApiService.get(nodeId, accessToken).enqueue(new Callback<GraphApiNode>() {
 
             @Override
-            public void onResponse(Call<GraphApiNode> call, Response<GraphApiNode> response) {
+            public void onResponse(@NonNull Call<GraphApiNode> call, @NonNull Response<GraphApiNode> response) {
                 GraphApiNode node = response.body();
-                Toast.makeText(ExperimentActivity.this, String.valueOf(node), Toast.LENGTH_LONG).show();
+                mGraphInfoText.setText(String.valueOf(node));
             }
 
             @Override
-            public void onFailure(Call<GraphApiNode> call, Throwable t) {
+            public void onFailure(@NonNull Call<GraphApiNode> call, @NonNull Throwable t) {
                 String msg = format("Error getting node [%s]", nodeId);
                 Toast.makeText(ExperimentActivity.this, msg, Toast.LENGTH_LONG).show();
                 Log.e(ExperimentActivity.class.getSimpleName(), msg, t);
