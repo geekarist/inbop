5029833c08405550d5f51f54e16c36e607528c9b
cpcp <geekarist@users.noreply.github.com>
2017-08-22 07:59:14 +0200

    Don't clone repo to get jks

--- generate-signed-apk.sh
+++ generate-signed-apk.sh
@@ -2,7 +2,4 @@
 
 set -eux
 
-git clone git@github.com:geekarist/android-private-files.git
-mkdir -p private
-cp android-private-files/me-cpele-android-keystore.jks private/
 ./gradlew assembleProductionRelease
